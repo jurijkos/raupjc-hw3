a543e8e8e6171a8eccc98064b56f982552ca5759
Jurij <jurij.kos1996@live.com>
2017-12-29 12:19:09 +0100

    pokusaj2

--- TodoWebApp/Controllers/TodoController.cs
+++ TodoWebApp/Controllers/TodoController.cs
@@ -1,23 +1,82 @@
-﻿using System;
+
+using System;
+
 using System.Collections.Generic;
+
 using System.Linq;
+
+using System.Net;
+
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using TodoRepository;
+using TodoWebApp.Models;
+using TodoWebApp.Models.TodoViewModels;
 
 namespace TodoWebApp.Controllers
 {
+    [Authorize]
     public class TodoController : Controller
     {
-        // GET: /Todo
-        public string Index()
+        private readonly ITodoRepository _repository;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public TodoController(ITodoRepository repository, UserManager<ApplicationUser> userManager)
         {
-            return "This is my default action...";
+            _repository = repository;
+            _userManager = userManager;
         }
-        //
-        //GET: /Todo/Welcome
-        public string Welcome()
+
+        public async Task<IActionResult> Index()
         {
-            return "This is the welcome action method...";
+            var activeTodos = _repository.GetActive(await GetCurrentUserIdAsync());
+            return View(activeTodos);
+        }
+
+
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddTodoViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var todoItem = new TodoItem(model.Text, await GetCurrentUserIdAsync());
+                _repository.Add(todoItem);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+
+
+        public async Task<IActionResult> Completed()
+        {
+            var completedTodos = _repository.GetCompleted(await GetCurrentUserIdAsync());
+            return View(completedTodos);
+        }
+
+
+
+        public async Task<IActionResult> CompleteTodo(Guid todoId)
+        {
+            _repository.MarkAsCompleted(todoId, await GetCurrentUserIdAsync());
+            return RedirectToAction("Index");
+        }
+
+
+
+        private async Task<Guid> GetCurrentUserIdAsync()
+        {
+            var user = await _userManager.GetUserAsync(HttpContext.User);
+            return new Guid(user.Id);
         }
     }
 }
--- TodoWebApp/Startup.cs
+++ TodoWebApp/Startup.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Interfaces;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using TodoRepository;
 using TodoWebApp.Data;
 using TodoWebApp.Models;
 using TodoWebApp.Services;
@@ -16,17 +19,29 @@ namespace TodoWebApp
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
-            
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets();
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add framework services.
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
@@ -34,20 +49,27 @@ namespace TodoWebApp
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
-            // Add application services.
-            services.AddTransient<IEmailSender, EmailSender>();
-
             services.AddMvc();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+
+            services.AddTransient<ITodoRepository, TodoSqlRepository>();
+            services.AddScoped<TodoDbContext>(t => new TodoDbContext(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
                 app.UseDatabaseErrorPage();
+                app.UseBrowserLink();
             }
             else
             {
@@ -56,7 +78,9 @@ namespace TodoWebApp
 
             app.UseStaticFiles();
 
-            app.UseAuthentication();
+            app.UseIdentity();
+
+            // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
 
             app.UseMvc(routes =>
             {
